d78903b36dc1c22eb1a0774ad751f46f0119f729
Lyubomir Krusharski <lubomir_krusharski@yahoo.com>
2017-05-16 20:45:17 +0300

    Small change in text output

--- TestGitHub/TestGit/Program.cs
+++ TestGitHub/TestGit/Program.cs
@@ -7,7 +7,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub");
+            Console.WriteLine("Hello from test GitHub");
         }
     }
 }
