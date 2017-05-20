8fb0611115f7d1a64ba4fdee520c6bd0b4965781
Lyubomir Krusharski <lubomir_krusharski@yahoo.com>
2017-05-20 23:55:53 +0300

    ASCII code converter

---
+++ TestGitHub/ASCIICode/ASCIICodeConvert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace ASCIICode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineNumber = int.Parse(Console.ReadLine());
+
+            var allText = string.Empty;
+
+            for (int i = 0; i < lineNumber; i++)
+            {
+                int asciiCode = int.Parse(Console.ReadLine());
+                var toChar = (char)asciiCode;
+                allText += toChar;
+            }
+
+            Console.WriteLine(allText);
+        }
+    }
+}
