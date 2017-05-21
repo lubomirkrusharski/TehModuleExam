58920aa0266a819fb28b43776b92374849fce0a5
Lyubomir Krusharski <lubomir_krusharski@yahoo.com>
2017-05-21 14:50:47 +0300

    Program to check sign of integer number

---
+++ TestGitHub/MetodNumber/SignNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace SignNumber
+{
+    class SignNumber
+    {
+        static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var intType = TypeCheck(number);
+            PrintSign(intType);
+        }
+
+        static string TypeCheck(int number)
+        {
+            var type = string.Empty;
+            if (number < 0)
+            {
+                type =  "negative";
+            }
+            else if (number == 0)
+            {
+                type = "zero";
+            }
+            else if (number > 0)
+            {
+                type = "positive";
+            }
+
+            return type;
+        }
+
+        static void PrintSign(string intType)
+        {
+            Console.WriteLine($"The number 2 is {intType}.");
+        }
+    }
+}
